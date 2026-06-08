3e3227b6d494e295d8e1cfab7cbefceea1291748
mdshan18 <mdshanjgp@gmail.com>
2026-06-08 22:50:10 +0530

    Initial commit - file organizer project

---
+++ README.md
@@ -0,0 +1,33 @@
+#file Organizer Project
+
+A Simple Python project that automatically organizes files into folders.
+##features
+1. Moves image to Image folder
+2. Moves videos to Video folder
+3. Moves documents to Document folder
+4. Moves audio to Audio folder
+
+##Technologies Used
+1. Python 
+2. Linux
+3. Git
+4. Github
+
+##How to run
+
+Run the following commands:
+on bash
+python3 organizer.py
+
+Then enter the folder path you want to organize.
+
+Example
+Before:
+photo.jpg
+movie.mp4
+resime.pdf
+
+After:
+Images/
+Videos/
+Documents/
---
+++ organizer.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+folder_path = input("Folder path daalo: ")
+
+file_types = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv", ".avi"],
+    "Audio": [".mp3", ".wav"]
+}
+
+for file in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, file)
+
+    if os.path.isfile(file_path):
+        extension = os.path.splitext(file)[1].lower()
+
+        for folder_name, extensions in file_types.items():
+            if extension in extensions:
+                target_folder = os.path.join(folder_path, folder_name)
+
+                if not os.path.exists(target_folder):
+                    os.makedirs(target_folder)
+
+                shutil.move(
+                    file_path,
+                    os.path.join(target_folder, file)
+                )
+
+                print(f"{file} moved to {folder_name}")
